00aa4b29666aae16e4095482f805dba8cf4635b1
João Chrisóstomo <joao.chrisostomo@gmail.com>
2022-08-25 22:15:56 -0300

    aula 05 - vectors

---
+++ a05_vectors.R
@@ -0,0 +1,21 @@
+#----------------------------------------#
+#Title: Aula 05 - Vectors
+#Author: João Chrisóstomo Ribeiro
+#----------------------------------------#
+
+# Definindo um vetor 1
+meuVetor1 <- c(1,2,3,"b")
+meuVetor1
+class (meuVetor1)
+
+# Definindo um vetor 2
+meuVetor2 <- c(1,2,3,4)
+meuVetor2
+class (meuVetor2)
+
+# Definindo um vetor 3 - intervalo A:B
+meuVetor3 <- 1:8
+meuVetor3
+# Definindo um vetor 4 - sequência entre A,B
+meuVetor4 <- seq(10,20)
+meuVetor4
